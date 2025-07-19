6c18ea811b73cd30fee76869adec0033c3980004
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-07-19 10:51:27 +0530

    Update App name

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Settings, Download, Image as ImageIcon } from "lucide-react";
 import logo from './assets/VS.jpg'
